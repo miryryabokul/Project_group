7c955397ca72417e5c3e9b853189e25ec9dc9349
dana <zinaezra@gmail.com>
2025-08-24 15:24:49 +0300

    jj

--- consts.py
+++ consts.py
@@ -1,10 +1,11 @@
 import pygame
 
 INITIAL_PLACE = (0, 0)
-DEFAULT_BACKGROUND_COLOR = (1,90,32)
+DEFAULT_BACKGROUND_COLOR = (138, 201, 38)
 SCREEN_WIDTH=800
 SCREEN_HEIGHT=600
-
+MAT_ROW = 25
+MAT_COL = 50
 FONT_NAME = "Calibri"
 OPENING_MESSAGE = "Welcome to the flag game' \nHave Fun!"
 OPENING_FONT_SIZE = int(0.005 * SCREEN_WIDTH)
@@ -12,7 +13,9 @@ BLACK = (0, 0, 0)
 OPENING_MESSAGE_COLOR = BLACK
 OPENING_MESSAGE_LOCATION = (20, 0)
 
-FLAG_LOCATION = (SCREEN_HEIGHT - 30)
+FLAG_LOCATION = (SCREEN_HEIGHT - 30, SCREEN_WIDTH - 40)
+X_LOCATION = 0
+Y_LOCATION = 0
 
 
 GRASS = pygame.image.load('grass.png')
@@ -30,5 +33,6 @@ INJURY = pygame.transform.scale(INJURY, (20, 40))
 MINE = pygame.image.load('mine.png')
 MINE = pygame.transform.scale(MINE, (30, 10))
 
-MINE_IMG = pygame.image.load('mine.png')
-MINE_IMG = pygame.transform.scale(MINE_IMG, (30, 10))
+FLAG = pygame.image.load('flag.png')
+FLAG = pygame.transform.scale(FLAG, (40, 30))
+
--- main.py
+++ main.py
@@ -1,23 +1,44 @@
 import pygame
 import screen
+import consts
+from consts import X_LOCATION
 
 
 def main():
     pygame.init()
     screen.normal_screen()
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                break
+
+
+
+
     pygame.quit()
 
 
+def handle_user_event():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ENTER:
+            # switch for 1 sec to the dark screen
 
-def key_control ():
-    key_input = input()
-    check = pygame.key.get_focused(key_input)
-    if check:
-        pygame.key.get_mods(key_input)
+            elif event.key == pygame.K_RIGHT:
+               if consts.X_LOCATION != consts.MAT_COL - 1:
+                   consts.X_LOCATION += 1
+
+            elif event.key == pygame.K_LEFT:
+                if consts.X_LOCATION != 0:
+                    consts.X_LOCATION -= 1
+
+            elif event.key == pygame.K_DOWN:
+                if consts.Y_LOCATION != consts.MAT_ROW - 1:
+                    consts.Y_LOCATION += 1
+
+            elif event.key == pygame.K_UP:
+                if consts.Y_LOCATION != 0:
+                    consts.Y_LOCATION -= 1
+
 
 
 if __name__ == "__main__":
