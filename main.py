86cb059ed680610adf7a88b2ac88950f25409ebe
dana <zinaezra@gmail.com>
2025-08-24 11:43:12 +0300

    mimimi

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+def main():
+    pygame.init()
+from pynput import keyboard
+from pynput.keyboard import Key
+
+def on_press(key):
+    #handle pressed keys
+    pass
+
+def on_release(key):
+    #handle released keys
+    if(key==Key.enter):
+        function_x()
+
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
+
+
+if __name__ == "__main__":
+    main()
+
+
