61cefaf00ef403f83251feaa5b2883b30b81804a
miryryabokul <miry.ryabokul@gmail.com>
2025-08-24 12:35:14 +0300

    Merge branch 'main' of https://github.com/miryryabokul/Project_group

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import pygame
+
 INITIAL_PLACE = (0, 0)
 DEFAULT_BACKGROUND_COLOR = (138, 201, 38)
 SCREEN_WIDTH=800
@@ -8,4 +10,5 @@ OPENING_FONT_SIZE = int(0.005 * SCREEN_WIDTH)
 BLACK = (0, 0, 0)
 OPENING_MESSAGE_COLOR = BLACK
 OPENING_MESSAGE_LOCATION =
-
+look_1 = pygame.image.load('data\\png\\look1.png').convert_alpha()
+look_1 = pygame.transform.scale(look_1, (new_width, new_height))
