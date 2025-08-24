94202b295e283c57fc1bb1ff43b09cd046f2ab8f
dana <zinaezra@gmail.com>
2025-08-24 11:52:12 +0300

    jj

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
-
-def main():
-    pygame.init()
 from pynput import keyboard
 from pynput.keyboard import Key
 
+def main():
+    pygame.init()
+
 def on_press(key):
-    #handle pressed keys
-    pass
+        #handle pressed keys
+        pass
 
 def on_release(key):
     #handle released keys
@@ -15,7 +15,7 @@ def on_release(key):
         function_x()
 
 with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-    listener.join()
+        listener.join()
 
 
 if __name__ == "__main__":
