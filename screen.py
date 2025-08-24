73995414881b42ff2456e1d13e147082536ecce4
miryryabokul <miry.ryabokul@gmail.com>
2025-08-24 13:17:44 +0300

    Merge branch 'main' of https://github.com/miryryabokul/Project_group

--- screen.py
+++ screen.py
@@ -7,12 +7,19 @@ pygame.display.set_caption('Flag Game')
 
 def normal_screen():
     screen.fill(consts.DEFAULT_BACKGROUND_COLOR)
+    for i in range(20):
+        draw_random(grass)
 
-def random_bush():
-    bush_x = random.randint(consts.SCREEN_WIDTH+1)
-    bush_y = random.randint(consts.SCREEN_HEIGHT+1)
-    screen.bilt(grass,(bush_x,bush_y))
+def draw_random(picture):
+    x = random.randint(consts.SCREEN_WIDTH + 1)
+    y = random.randint(consts.SCREEN_HEIGHT + 1)
+    screen.bilt(picture, (x, y))
 
-
-pygame.image.load(soldier.png)
+def matrix_screen():
+    pygame.draw.rect(screen,color,(x,y))
+    for row in matrix:
+        for item in row:
+            pygame.draw.rect(screen, color, (row,item), 1)
+    for j in range(20):
+        draw_random(explotion)
 
