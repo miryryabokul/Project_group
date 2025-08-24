db1192a5bd72695a1e6914f5d85bed3b5bca071c
miryryabokul <miry.ryabokul@gmail.com>
2025-08-24 11:49:57 +0300

    files

---
+++ screen.py
@@ -0,0 +1,8 @@
+import pygame
+import consts
+screen = pygame.display.set_mode((consts.SCREEN_WIDTH,consts.SCREEN_HEIGHT))
+
+def normal_screen():
+    screen.fill(consts.DEFAULT_BACKGROUND_COLOR)
+
+
