64c4bb5a03bd1576649e0b3538eff2310eee3b99
dana <zinaezra@gmail.com>
2025-08-25 14:27:20 +0300

    jj

--- game_field.py
+++ game_field.py
@@ -1,20 +1,25 @@
 import consts
-# MAT_ROW = 25
-# MAT_COL = 50
-game_field = []
-
-dict_field = {}
-for row in range (1, consts.MAT_ROW +1 ):
-    x = []
-    for col in range (1, consts.MAT_COL + 1):
-        dict_cal={}
-        dict_cal["x_location"]=row*30
-        dict_cal["y_location"]=col*30
-        dict_cal["role"]="EMPTY"
-        x.append(dict_cal)
-    game_field.append(x)
+import pygame
+import sys
 
 
+def create_field (game_field):
+    for row in range (1, consts.MAT_ROW +1 ):
+        x = []
+        for col in range (1, consts.MAT_COL + 1):
+            dict_cal={}
+            dict_cal["x_location"]=row*30
+            dict_cal["y_location"]=col*30
+            dict_cal["role"]="EMPTY"
+            x.append(dict_cal)
+        game_field.append(x)
+    return game_field
 
-print(game_field)
+
+def check_win (soldier, flag):
+    for i in range (soldier):
+        if soldier[-1].colliderect(flag):
+    #      print lose message for 3 sec
+            pygame.quit()
+            sys.exit()
 
