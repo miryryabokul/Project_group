c3eb8e34dafbec304eac510e577f6ca491880f02
dana <zinaezra@gmail.com>
2025-08-25 15:50:17 +0300

    jj

---
+++ soldier.py
@@ -0,0 +1,29 @@
+import game_field
+from game_field import check_win
+
+solider_location = [0, 0]
+def search_flag ():
+    for i in range (solider_location):
+        if solider_location[0] == 720 and solider_location[1] == 1410 :
+            game_field.check_win()
+        if solider_location[0] == 750 and solider_location[1] == 1410:
+            game_field.check_win()
+
+        if solider_location[0] == 720 and solider_location[1] == 1440 :
+            game_field.check_win()
+        if solider_location[0] == 750 and solider_location[1] == 1440:
+            game_field.check_win()
+
+        if solider_location[0] == 720 and solider_location[1] == 1470 :
+            game_field.check_win()
+        if solider_location[0] == 750 and solider_location[1] == 1470:
+            game_field.check_win()
+    return
+
+def search_mine ():
+    for j in game_field:
+        if solider_location[0] and solider_location[1]+120  == game_field[j][3]:
+            game_field.check_win()
+        if solider_location[0]+60 and solider_location[1] == game_field[j][3]:
+            game_field.check_win()
+    return
